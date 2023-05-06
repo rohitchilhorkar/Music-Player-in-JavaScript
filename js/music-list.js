9e70bc18975b458c08cc5947ff244805f7a27226
rdc07 <rdchilhorkar@gmail.com>
2023-05-06 20:26:07 +0530

    Music Player

---
+++ js/music-list.js
@@ -0,0 +1,342 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in  formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in  formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+let energeticSongs = [
+  {
+    name: "Amhi_Lagnalu",
+    artist: "Jordan Schor",
+    img: "Amhi_Lagnalu",
+    src: "Amhi_Lagnalu"
+  },
+  {
+    name: "Amhi_Nahi_Jaa",
+    artist: "",
+    img: "Amhi_Nahi_Jaa",
+    src: "Amhi_Nahi_Jaa"
+  },
+  {
+    name: "Ararara",
+    artist: "",
+    img: "Ararara",
+    src: "Ararara"
+  },
+  {
+    name: "Bhayanak Atma",
+    artist: "",
+    img: "Bhayanak Atma",
+    src: "Bhayanak Atma"
+  },
+  {
+    name: "Blockbuster",
+    artist: "",
+    img: "Blockbuster",
+    src: "Blockbuster"
+  },
+   {
+    name: "Govyachya_Kinaryavar",
+    artist: "",
+    img: "Govyachya_Kinaryavar",
+    src: "Govyachya_Kinaryavar"
+  },
+  {
+    name: "Haaye Garmi",
+    artist: "",
+    img: "Haaye Garmi",
+    src: "Haaye Garmi"
+  },
+  {
+    name: "Ilahi",
+    artist: "",
+    img: "Ilahi",
+    src: "Ilahi"
+  },
+  {
+    name: "Malhari",
+    artist: "",
+    img: "Malhari",
+    src: "Malhari"
+  },
+  {
+    name: "Suit_Suit",
+    artist: "",
+    img: "Suit_Suit",
+    src: "Suit_Suit"
+  }
+]
+
+let oldSongs = [ 
+  {
+    name: "Kya Khoob Lagte Ho",
+    artist: "",
+    img: "Kya Khoob Lagte Ho",
+    src: "Kya Khoob Lagte Ho"
+  },
+  {
+    name: "Aise Na Mujhe Tum",
+    artist: "",
+    img: "Aise Na Mujhe Tum",
+    src: "Aise Na Mujhe Tum"
+  },
+  {
+    name: "Ek_Ladki_Ko_Dekha_To",
+    artist: "",
+    img: "Ek_Ladki_Ko_Dekha_To",
+    src: "Ek_Ladki_Ko_Dekha_To"
+  },
+  {
+    name: "Jab_Koi_Baat",
+    artist: "",
+    img: "Jab_Koi_Baat",
+    src: "Jab_Koi_Baat"
+  },
+  {
+    name: "Kya Khoob Lagte Ho",
+    artist: "",
+    img: "Kya Khoob Lagte Ho",
+    src: "Kya Khoob Lagte Ho"
+  },
+  {
+    name: "Main_Duniya_Bhula_Doonga_Aashiqui",
+    artist: "",
+    img: "Main_Duniya_Bhula_Doonga_Aashiqui",
+    src: "Main_Duniya_Bhula_Doonga_Aashiqui"
+  },
+  {
+    name: "Pehla Nasha",
+    artist: "",
+    img: "Pehla Nasha",
+    src: "Pehla Nasha"
+  },
+  {
+    name: "Sandese Aate Hain - Border",
+    artist: "",
+    img: "Sandese Aate Hain - Border",
+    src: "Sandese Aate Hain - Border"
+  },
+  {
+    name: "Subah Se Lekar",
+    artist: "",
+    img: "Subah Se Lekar",
+    src: "Subah Se Lekar"
+  },
+  {
+    name: "Tere_Naam_Liya",
+    artist: "",
+    img: "Tere_Naam_Liya",
+    src: "Tere_Naam_Liya"
+  }
+]
+let sadMusic = [
+  {
+    name: "Aayat",
+    artist: "",
+    img: "Aayat",
+    src: "Aayat"
+  },
+  {
+    name: "Akon LONELY",
+    artist: "",
+    img: "Akon LONELY",
+    src: "Akon LONELY"
+  },
+  {
+    name: "Bulleya",
+    artist: "",
+    img: "Bulleya",
+    src: "Bulleya"
+  },
+  {
+    name: "Channa Mereya",
+    artist: "",
+    img: "Channa Mereya",
+    src: "Channa Mereya"
+  },
+  {
+    name: "Hamdard",
+    artist: "",
+    img: "Hamdard",
+    src: "Hamdard"
+  },
+  {
+    name: "Kitida Navyane",
+    artist: "",
+    img: "Kitida Navyane",
+    src: "Kitida Navyane"
+  },
+  {
+    name: "Le Chala",
+    artist: "",
+    img: "Le Chala",
+    src: "Le Chala"
+  },
+  {
+    name: "Mujh me khi",
+    artist: "",
+    img: "Mujh me khi",
+    src: "Mujh me khi"
+  },
+  {
+    name: "Phir Mohabbat",
+    artist: "",
+    img: "Phir Mohabbat",
+    src: "Phir Mohabbat"
+  },
+  {
+    name: "Saiyyan",
+    artist: "",
+    img: "Saiyyan",
+    src: "Saiyyan"
+  }
+]
+
+let happyMusic = [
+  {
+    name: "Saavar Re Mana",
+    artist: "",
+    img: "Saavar Re Mana",
+    src: "Saavar Re Mana"
+  },
+  {
+    name: "Mala Ved Laagale",
+    artist: "",
+    img: "Mala Ved Laagale",
+    src: "Mala Ved Laagale"
+  },
+  {
+    name: "Man_Udhan_Varyache",
+    artist: "",
+    img: "Man_Udhan_Varyache",
+    src: "Man_Udhan_Varyache"
+  },
+  {
+    name: "Mast_Magan",
+    artist: "",
+    img: "Mast_Magan",
+    src: "Mast_Magan"
+  },
+  {
+    name: "Roz Mala Visrun Me",
+    artist: "",
+    img: "Roz Mala Visrun Me",
+    src: "Roz Mala Visrun Me"
+  },
+  {
+    name: "Love Me Thoda Aur",
+    artist: "",
+    img: "Love Me Thoda Aur",
+    src: "Love Me Thoda Aur"
+  },
+  {
+    name: "Kadhi Tu",
+    artist: "",
+    img: "Kadhi Tu",
+    src: "Kadhi Tu"
+  },
+  {
+    name: "Inkem_Inkem_Inkem_Kaavaale",
+    artist: "",
+    img: "Inkem_Inkem_Inkem_Kaavaale",
+    src: "Inkem_Inkem_Inkem_Kaavaale"
+  },
+  {
+    name: "Enna_Sona",
+    artist: "",
+    img: "Enna_Sona",
+    src: "Enna_Sona"
+  },
+  {
+    name: "Ed Sheeran - Perfect",
+    artist: "",
+    img: "Ed Sheeran - Perfect",
+    src: "Ed Sheeran - Perfect"
+  },
+  {
+    name: "Dil Diyan Gallan",
+    artist: "",
+    img: "Dil Diyan Gallan",
+    src: "Dil Diyan Gallan"
+  },
+  {
+    name: "Albela Sajan",
+    artist: "",
+    img: "Albela Sajan",
+    src: "Albela Sajan"
+  },
+  {
+    name: "Aashiyan - Barfi",
+    artist: "",
+    img: "Aashiyan - Barfi",
+    src: "Aashiyan - Barfi"
+  }
+]
+
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1"
+  },  
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Baazi",
+    artist: "King Rocco",
+    img: "music-2",
+    src: "Baazi"
+  },
+  {
+    name: "Oops",
+    artist: "King Rocco",
+    img: "music-2",
+    src: "Oops"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Pablo",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "Pablo"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  }
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in  formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in  formate and it's inside the songs folder of this project folder"
+  // }
+];
